9908ed31b5fc82e51d79b05866cf45af9d806eb2
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2020-01-26 20:21:13 +0200

    Day-22: do the vm work

--- vm/include/vm/Op.h
+++ vm/include/vm/Op.h
@@ -4,16 +4,16 @@
 
 namespace vm
 {
-	enum Op: uint8_t
+	enum Op: int8_t
 	{
 		// illegal opcode
 		Op_IGL,
 
-		// LOAD [dst] [constant]
-		Op_LOAD8,
-		Op_LOAD16,
-		Op_LOAD32,
-		Op_LOAD64,
+		// MOV [dst] [src]
+		Op_MOV8,
+		Op_MOV16,
+		Op_MOV32,
+		Op_MOV64,
 
 		// ADD [dst + op1] [op2]
 		Op_ADD8,
@@ -21,23 +21,11 @@ namespace vm
 		Op_ADD32,
 		Op_ADD64,
 
-		// IMMADD [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMADD8,
-		Op_IMMADD16,
-		Op_IMMADD32,
-		Op_IMMADD64,
-
 		// SUB [dst + op1] [op2]
 		Op_SUB8,
 		Op_SUB16,
 		Op_SUB32,
 		Op_SUB64,
-
-		// IMMSUB [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMSUB8,
-		Op_IMMSUB16,
-		Op_IMMSUB32,
-		Op_IMMSUB64,
 
 		// MUL [dst + op1] [op2]
 		Op_MUL8,
@@ -45,23 +33,11 @@ namespace vm
 		Op_MUL32,
 		Op_MUL64,
 
-		// IMMMUL [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMMUL8,
-		Op_IMMMUL16,
-		Op_IMMMUL32,
-		Op_IMMMUL64,
-
 		// IMUL [dst + op1] [op2]
 		Op_IMUL8,
 		Op_IMUL16,
 		Op_IMUL32,
 		Op_IMUL64,
-
-		// IMMIMUL [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMIMUL8,
-		Op_IMMIMUL16,
-		Op_IMMIMUL32,
-		Op_IMMIMUL64,
 
 		// DIV [dst + op1] [op2]
 		Op_DIV8,
@@ -69,23 +45,11 @@ namespace vm
 		Op_DIV32,
 		Op_DIV64,
 
-		// IMMDIV [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMDIV8,
-		Op_IMMDIV16,
-		Op_IMMDIV32,
-		Op_IMMDIV64,
-
 		// IDIV [dst + op1] [op2]
 		Op_IDIV8,
 		Op_IDIV16,
 		Op_IDIV32,
 		Op_IDIV64,
-
-		// IMMIDIV [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMIDIV8,
-		Op_IMMIDIV16,
-		Op_IMMIDIV32,
-		Op_IMMIDIV64,
 
 		// unsigned compare
 		// CMP [op1] [op2]
@@ -94,24 +58,12 @@ namespace vm
 		Op_CMP32,
 		Op_CMP64,
 
-		// IMMCMP [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMCMP8,
-		Op_IMMCMP16,
-		Op_IMMCMP32,
-		Op_IMMCMP64,
-
 		// signed compare
 		// ICMP [op1] [op2]
 		Op_ICMP8,
 		Op_ICMP16,
 		Op_ICMP32,
 		Op_ICMP64,
-
-		// IMMICMP [dst + op1] [immediate argument (8-bit, 16-bit, 32-bit, 64-bit number)]
-		Op_IMMICMP8,
-		Op_IMMICMP16,
-		Op_IMMICMP32,
-		Op_IMMICMP64,
 
 		// jump unconditionall
 		// JMP [offset 64-bit]
@@ -141,20 +93,6 @@ namespace vm
 		// JGE [offset 64-bit]
 		Op_JGE,
 
-		// reads data from the src address register into the specified dst register
-		// READ [dst] [src]
-		Op_READ8,
-		Op_READ16,
-		Op_READ32,
-		Op_READ64,
-
-		// writes data from the src register into the specified dst address register
-		// WRITE [dst] [src]
-		Op_WRITE8,
-		Op_WRITE16,
-		Op_WRITE32,
-		Op_WRITE64,
-
 		// pushes the register into the stack and increment it
 		// PUSH [register]
 		Op_PUSH,
@@ -175,11 +113,54 @@ namespace vm
 		// C_CALL [unsigned 64-bit index into c_proc array in core]
 		Op_C_CALL,
 
-		// TEMPORARY Op
-		// prints a debug string
-		// DEBUGSTR [register = address unsigned 64-bit]
-		Op_DEBUGSTR,
-
 		Op_HALT,
 	};
+
+	// EXT = 0123 4567
+	// EXT[0] = indicates this is an extension
+	// EXT[1, 2] = addressing mode, choose from [reg, imm, mem]
+	// EXT[3] = shifted addressing mode
+	// EXT[4, 5] = reserved bits -> always 0
+	// EXT[6, 7] = scale mode, choose from [1, 2, 4, 8] scaling
+	// default addressing mode is registers if you want to change that
+	// add two extension bytes before the opcode itself, [dst ext] [src ext] [opcode] [dst] [src]
+	// for default addressing mode you omit the extension bytes [opcode] [dst] [src]
+
+	enum ADDRESS_MODE: uint8_t
+	{
+		ADDRESS_MODE_REG,
+		ADDRESS_MODE_IMM,
+		ADDRESS_MODE_MEM,
+	};
+
+	enum SCALE_MODE: uint8_t
+	{
+		SCALE_MODE_1X,
+		SCALE_MODE_2X,
+		SCALE_MODE_4X,
+		SCALE_MODE_8X,
+	};
+
+	struct Ext
+	{
+		bool is_ext : 1;
+		ADDRESS_MODE address_mode : 2;
+		bool is_shifted : 1;
+		uint8_t reserved : 2;
+		SCALE_MODE scale_mode : 2;
+	};
+
+	static_assert(sizeof(Ext) == sizeof(uint8_t), "Ext should be of size 1");
+
+	inline static Ext
+	ext_from_byte(uint8_t b)
+	{
+		return *(Ext*)&b;
+	}
+
+	inline static uint8_t
+	ext_to_byte(Ext e)
+	{
+		return *(uint8_t*)&e;
+	}
 }
